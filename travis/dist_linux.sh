1cc0b3ebd394f10f156f1dd7802a4b26475c525b
cakebake <cakebake.dev@gmail.com>
2019-02-18 11:51:54 +0100

    Fixes permissions

--- travis/dist_linux.sh
+++ travis/dist_linux.sh
@@ -3,6 +3,7 @@
 docker pull $DOCKER_IMAGE
 docker run --rm \
   -v $PWD:/workdir \
-  --user $UID:$GID \
+  -e "FINAL_USER=$UID" \
+  -e "FINAL_GROUP=$GID" \
   $DOCKER_IMAGE \
   /workdir/travis/build_linux.sh
