dc2b13ebe50fd5ca0ed17ba19b645051f59cad92
Jens A <cakebake.dev@gmail.com>
2019-02-19 21:32:06 +0100

    Adding missing libs for staticx

--- travis/build_linux.sh
+++ travis/build_linux.sh
@@ -20,15 +20,14 @@ apt-get update -qq && apt-get install -qq -y \
 
 pip3 install --upgrade --no-cache-dir \
   'WeasyPrint==44' \
+  'staticx==0.6.0' \
   'pyinstaller==3.4'
 
 weasyprint --version
+staticx --version
 pyinstaller --version
 
 cd /tmp
-
-PYTHONHASHSEED=123456
-export PYTHONHASHSEED
 
 pyinstaller /usr/local/bin/weasyprint \
   --name weasyprint \
@@ -40,12 +39,9 @@ pyinstaller /usr/local/bin/weasyprint \
   --add-data '/usr/local/lib/python3.7/dist-packages/weasyprint/VERSION:.' \
   --add-data '/usr/local/lib/python3.7/dist-packages/cairocffi/VERSION:cairocffi'
 
-# temp patch staticx because of libjpeg not found
-apt-get install -qq -y scons musl-tools
-CC=/usr/bin/musl-gcc pip3 install https://github.com/JonathonReinhart/staticx/archive/61-fix-rpath-origin.zip
+# temp fix because libs of executable not found by staticx
 sed -i 's/# Some shared objs might have no DT_NEEDED tags (see issue #67)/print("ldd, unexpected lin in ldd output: " + line)\n            continue/g' \
   /usr/local/lib/python3.7/dist-packages/staticx/elf.py
-
-staticx --version
+cp -vf /workdir/travis/bin/* /lib/x86_64-linux-gnu
 
 staticx /tmp/dist/weasyprint /workdir/dist/weasyprint
