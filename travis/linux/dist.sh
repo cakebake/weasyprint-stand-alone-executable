a1c7d315f23d492a71df118d9c607d7f755dea97
cakebake <cakebake.dev@gmail.com>
2019-03-11 13:41:15 +0100

    Fixes path to Dockerfile

--- travis/linux/dist.sh
+++ travis/linux/dist.sh
@@ -2,13 +2,13 @@
 
 # generate executable
 docker build --quiet \
+  --file ./travis/linux/Dockerfile \
   --pull \
   --tag wpimg \
   --build-arg WEASYPRINT_VERSION=$WEASYPRINT_VERSION \
   --build-arg STATICX_VERSION=$STATICX_VERSION \
   --build-arg PYINSTALLER_VERSION=$PYINSTALLER_VERSION \
   .
-
 docker run --rm \
   -e "HOST_OWNER_UID=$UID" \
   -e "HOST_OWNER_GID=$GID" \
